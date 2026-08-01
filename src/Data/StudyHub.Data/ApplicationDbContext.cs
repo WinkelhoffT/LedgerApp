46de9e630ee54a864b25f55f5a2ebadad7c283ee
WinkelhoffT <thili@sf-books.de>
2026-08-01 16:29:55 +0200

    Configure Semester entity in ApplicationDbContext
    Add the Semesters DbSet and EF Core mapping (table, key, required properties, unique name index) for the Semester entity.

--- src/Data/StudyHub.Data/ApplicationDbContext.cs
+++ src/Data/StudyHub.Data/ApplicationDbContext.cs
@@ -1,11 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using StudyHub.Logic.Domain.Courses;
+using StudyHub.Logic.Domain.Semesters;
 
 namespace StudyHub.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
     public DbSet<Course> Courses => Set<Course>();
+
+    public DbSet<Semester> Semesters => Set<Semester>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -36,6 +39,35 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
                 .IsRequired();
 
             builder.HasIndex(c => c.Name)
+                .IsUnique();
+        });
+
+        modelBuilder.Entity<Semester>(builder =>
+        {
+            builder.ToTable("Semesters");
+
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.Name)
+                .HasMaxLength(Semester.NameMaxLength)
+                .IsRequired();
+
+            builder.Property(s => s.StartDate)
+                .IsRequired();
+
+            builder.Property(s => s.EndDate)
+                .IsRequired();
+
+            builder.Property(s => s.IsArchived)
+                .IsRequired();
+
+            builder.Property(s => s.CreatedAt)
+                .IsRequired();
+
+            builder.Property(s => s.UpdatedAt)
+                .IsRequired();
+
+            builder.HasIndex(s => s.Name)
                 .IsUnique();
         });
     }
