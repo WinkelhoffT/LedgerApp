8f09e819d027bea3c0fbfebf57fa9070ad3e5b8d
WinkelhoffT <thili@sf-books.de>
2026-08-01 16:29:22 +0200

    Add SemesterDto and SemesterErrorCodes
    Data transfer contract and stable error codes used across the Business and Api layers for Semester CRUD.

---
+++ src/Logic/StudyHub.Logic.Business/Semesters/SemesterErrorCodes.cs
@@ -0,0 +1,9 @@
+namespace StudyHub.Logic.Business.Semesters;
+
+public static class SemesterErrorCodes
+{
+    public const string SemesterNotFound = "semester_not_found";
+    public const string DuplicateSemesterName = "duplicate_semester_name";
+    public const string SemesterValidationFailed = "semester_validation_failed";
+    public const string SemesterArchived = "semester_archived";
+}
